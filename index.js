1849e320194422ca1a1e5bb3a8c48e590c50b8a7
nour khoury <khourynour79@gmail.com>
2022-09-13 22:25:36 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+
+
+ 
+function writeCards(name,event){
+
+    for(let i =0 ;i<name.length;i++){
+        console.log(`Thank you ${name} for the wonderful ${event} gift`)
+        debugger;
+    }
+
+    return name,event;
+
+}
+
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
